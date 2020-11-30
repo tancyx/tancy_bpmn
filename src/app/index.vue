da88c4b6c33f3384586adf26ae26c62145410b8f
tancy <tancy@git.com>
2020-11-30 14:40:26 +0800

    none

--- src/app/index.vue
+++ src/app/index.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="container" ref="container"></div>
+  <div class="main">
+    <div :class="['box', theme]" ref="container" @mouseup="mouseupEvent" v-contextmenu:contextmenu></div>
+    
+    <contextmenu class="context-menu" ref="contextmenu" :theme="theme" :disabled="disabled">
+      <contextmenu-item class="context-menu-item">更多配置</contextmenu-item>
+    </contextmenu>
+
+    <el-dialog
+    title="属性配置"
+    :modal="true"
+    :visible.sync="dialogVisible">
+    
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-// import Metadata from "../util/Metadata";
+import Metadata from "../util/Metadata";
 // import configureModeler from "./bpmn/util/configure";
 // import { isFunction } from "min-dash";
 import CamundaBpmnModeler from "./bpmn/modeler";
 import bpmnXml from "./diagram.bpmn";
+import replaceIds from '@bpmn-io/replace-ids';
+import {generateId} from "../util"
+import { is } from "bpmn-js/lib/util/ModelUtil"
+import {
+  directive,
+  Contextmenu,
+  ContextmenuItem
+  // ContextmenuSubmenu
+  // ContextmenuGroup
+} from 'v-contextmenu/src'
+import 'v-contextmenu/dist/index.css'
 
 export default {
   name: "container",
+  components: {
+    Contextmenu,
+    ContextmenuItem
+    // ContextmenuSubmenu
+  },
+  directives: {
+    contextmenu: directive,
+  },
+  props: {
+    theme: {
+      type: String,
+      default: "default"
+    }
+  },
   data() {
-    return {};
+    return {
+      element: {},
+      disabled: true,
+      hoverElement: {},
+      dialogVisible: false
+    };
   },
   mounted() {
     this.$nextTick(() => {
@@ -21,75 +64,54 @@ export default {
   },
   methods: {
     async createModeler() {
-      // const xml = `<?xml version="1.0" encoding="UTF-8"?>
-      // <bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="Definitions_{{ ID }}" targetNamespace="http://bpmn.io/schema/bpmn" exporter="Camunda Modeler" exporterVersion="3.0.0-dev">
-      //   <bpmn:process id="Process_{{ ID:process }}" isExecutable="true">
-      //     <bpmn:startEvent id="StartEvent_1" />
-      //   </bpmn:process>
-      //   <bpmndi:BPMNDiagram id="BPMNDiagram_1">
-      //     <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_{{ ID:process }}">
-      //       <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">
-      //         <dc:Bounds x="179" y="159" width="36" height="36" />
-      //       </bpmndi:BPMNShape>
-      //     </bpmndi:BPMNPlane>
-      //   </bpmndi:BPMNDiagram>
-      // </bpmn:definitions>
-      // `;
-      const xml = bpmnXml;
+      const xml = replaceIds(bpmnXml, generateId);
       console.log(xml);
       const canvas = this.$refs.container;
-      // const { name, version } = Metadata;
+      Metadata.init({name: "tancy modeler", version: "1.0"});
+      const { name, version } = Metadata;
 
-      // const { getPlugins, onAction, onError } = props;
-
-      // // notify interested parties that modeler will be configured
-      // const handleMiddlewareExtensions = middlewares => {
-      //   onAction("emit-event", {
-      //     type: "bpmn.modeler.configure",
-      //     payload: {
-      //       middlewares
-      //     }
-      //   });
-      // };
-
-      // const { options, warnings } = configureModeler(
-      //   getPlugins,
-      //   {
-      //     exporter: {
-      //       name,
-      //       version
-      //     }
-      //   },
-      //   handleMiddlewareExtensions
-      // );
-
-      // if (warnings.length && isFunction(onError)) {
-      //   onError(
-      //     "Problem(s) configuring BPMN editor: \n\t" +
-      //       warnings.map(error => error.message).join("\n\t") +
-      //       "\n"
-      //   );
-      // }
-
+     
       const modeler = new CamundaBpmnModeler({
         container: canvas,
         exporter: {
-          name: "tancy modeler",
-          version: new Date().getMilliseconds()
+          name: name,
+          version: version
         },
         position: "absolute"
       });
-
-      try {
-        await modeler.importXML(xml);
-      } catch (error) {
-        console.log(error);
-      }
+      modeler.importXML(xml);
       console.log(modeler)
-      // const commandStack = modeler.get("commandStack");
+      const commandStack = modeler.get("commandStack");
+      console.log(commandStack);
+      // const canvasobj = modeler.get("canvas");
+      const eventBus = modeler.get("eventBus");
+      console.log(eventBus);
+      // eventBus.on("selection.changed", (e) => {
+      //   console.log(e);
+      //   if (e.newSelection && e.newSelection.length == 1) {
+      //     this.element = e.newSelection[0];
+      //   } else {
+      //     this.element = canvasobj._rootElement;
+      //   }
+      // });
+      // eventBus.on("element.hover", (e) => {
+      //   console.log(e);
+      //   this.hoverElement = e.element
+      // });
+
+      // eventBus.on("shape.move.hover", (e) => {
+      //   console.log(e);
+      //   this.hoverElement = e.element
+      // });
+      eventBus.on("element.dblclick", (e) => {
+        console.log(e);
+        this.dialogVisible = true;
+      });
+      
+
+      // eventBus.on("element.click", (e) => {console.log(e.element, is(e.element, "bpmn:UserTask"));})
 
       // const stackIdx = commandStack._stackIdx;
-
       // notify interested parties that modeler was created
       // onAction("emit-event", {
       //   type: "bpmn.modeler.created",
@@ -97,10 +119,65 @@ export default {
       //     modeler
       //   }
       // });
+    },
+    mouseupEvent(event) {
+      if (this.hoverElement && event.button == 2) {
+        console.log(this.hoverElement);
+        const { clientX, clientY } = event;
+        let position = {
+          top: clientY,
+          left: clientX
+        };
+        if (!is(this.hoverElement, "bpmn:Process")) {
+          const { x, y, width, height } = this.hoverElement;
+          const endx = x + width;
+          const endy = y + height;
+          if (!(clientX > x && clientX < endx && clientY > y && clientY < endy)) {
+            return;
+          }
+        }
+        if (is(this.hoverElement, "bpmn:SequenceFlow")) {
+          const reactWidth = 10;
+          let showContex = false;
+          const waypoints = this.hoverElement.waypoints;
+          for (let index = 0; index < waypoints.length - 1; index++) {
+            const currentPoint = waypoints[index];
+            const nextPoint = waypoints[index + 1];
+            const { currentx, currenty } = currentPoint;
+            const { nextx, nexty } = nextPoint;
+            if (currentx == nextx) {
+              if (Math.abs(currentx - clientX) <= reactWidth && clientY != Math.min(currenty, clientY, nexty) && clientY != Math.max(currenty, clientY, nexty)) {
+                showContex = true;
+                break;
+              }
+            }
+            if (currenty == nexty) {
+              if (Math.abs(currenty - clientY) <= reactWidth && clientX != Math.min(currentx, clientX, nextx) && clientX != Math.max(currentx, clientX, nextx)) {
+                showContex = true;
+                break;
+              }
+            }
+          }
+          if (!showContex) {
+            return;
+          }
+        }
+        this.$refs.contextmenu.show(position)
+      }
     }
   }
 };
 </script>
 
 <style scoped>
+.context-menu {
+  width: 100px !important;
+}
+.context-menu-item {
+  height: 30px !important;
+}
+.dialog-class {
+  width: 500px;
+  height: 300px;
+}
 </style>
